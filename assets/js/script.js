91038f5a3c6e58931fff42f37b357c2c63a500ca
AmarDange <amardange218@gmail.com>
2023-02-18 13:54:49 +0000

    Add onkeydown statement in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-console.log("connected!")
+document.onkeydown = function(e){
+    console.log("key code is: ",e.keyCode)
+    if (e.keyCode==38){
+        deer = document.querySelector('.deer');
+        deer.classList.add('animatedeer');
+        setTimeout(() => {
+            deer.classList.remove('animatedeer')
+        }, (700));
+    }
+
+
+}
